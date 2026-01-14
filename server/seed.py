a0bc687a4a0ea6ffd03feea53cde4b81193ef45b
Marvin Kioko <kiokomarvin7@gmail.com>
2026-01-14 20:01:16 +0300

    first commit

--- server/seed.py
+++ server/seed.py
@@ -1,31 +1,51 @@
+# server/app.py
 #!/usr/bin/env python3
-#server/seed.py
-from random import choice as rc
-from faker import Faker
 
-from app import app
+from flask import Flask, make_response
+from flask_migrate import Migrate
+
 from models import db, Pet
 
-with app.app_context():
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
-    # Create and initialize a faker generator
-    fake = Faker()
+migrate = Migrate(app, db)
+db.init_app(app)
 
-    # Delete all rows in the "pets" table
-    Pet.query.delete()
+@app.route('/')
+def index():
+    body = {'message': 'Welcome to the pet directory!'}
+    return make_response(body, 200)
 
-    # Create an empty list
-    pets = []
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id == id).first()
 
-    species = ['Dog', 'Cat', 'Chicken', 'Hamster', 'Turtle']
+    if pet:
+        body = {'id': pet.id,
+                'name': pet.name,
+                'species': pet.species}
+        status = 200
+    else:
+        body = {'message': f'Pet {id} not found.'}
+        status = 404
 
-    # Add some Pet instances to the list
-    for n in range(10):
-        pet = Pet(name=fake.first_name(), species=rc(species))
-        pets.append(pet)
+    return make_response(body, status)
 
-    # Insert each Pet in the list into the "pets" table
-    db.session.add_all(pets)
+@app.route('/species/<string:species>')
+def pet_by_species(species):
+    pets = []  # array to store a dictionary for each pet
+    for pet in Pet.query.filter_by(species=species).all():
+        pet_dict = {'id': pet.id,
+                    'name': pet.name,
+                    }
+        pets.append(pet_dict)
+    body = {'count': len(pets),
+            'pets': pets
+            }
+    return make_response(body, 200)
 
-    # Commit the transaction
-    db.session.commit()
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
